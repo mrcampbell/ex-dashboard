ff30e85b7a896651c689cd50461dc49d3c6661d3
Mike Campbell <mike.campbell@divvypay.com>
2021-09-02 19:47:47 -0600

    added tailwind

---
+++ assets/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "../lib/**/*.eex",
+      "../lib/**/*.leex",
+      "../lib/**/*_view.ex"
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+    options: {
+      whitelist: [/phx/, /nprogress/]
+    }
+  }
+}
